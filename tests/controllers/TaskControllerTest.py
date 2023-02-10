2447ae31989de419a7a64acba44c546daf0acbc7
threezinedine <threezinedine@gmail.com>
2023-02-11 01:37:40 +0700

    add first test for creating new task for existed user.

--- tests/controllers/TaskControllerTest.py
+++ tests/controllers/TaskControllerTest.py
@@ -3,11 +3,22 @@ import unittest
 from app.utils.database import (
     clear_data,
 )
+from constants import HTTP_201_CREATED
 from tests.database import (
     get_testing_session,
 )
+from constants.test.user import (
+    FIRST_USER_USERID,
+)
+from constants.test.task import (
+    FIRST_TASK,
+    FIRST_TASK_TASK_NAME,
+    FIRST_TASK_TASK_DESCRIPTION,
+    FIRST_TASK_TASK_PLANNED_DATE,
+)
 from app.controllers.TaskController import TaskController
 from app.controllers.UserController import UserController
+from tests.utils import assertStatus, assertTaskWithDict, createFirstUserBy
 
 
 class TaskControllerTest(unittest.TestCase):
@@ -19,5 +30,15 @@ class TaskControllerTest(unittest.TestCase):
     def tearDown(self):
         clear_data(self.session)
 
-    def test_first_test(self):
-        assert True
+    def test_given_a_user_exist_when_create_a_task_with_that_user_then_return_STATUS_OK_and_that_task(self):
+        createFirstUserBy(self.user_controller)
+
+        status, task = self.task_controller.create_new_task(
+            userId=FIRST_USER_USERID,
+            taskName=FIRST_TASK_TASK_NAME,
+            taskDescription=FIRST_TASK_TASK_DESCRIPTION,
+            taskPlannedDate=FIRST_TASK_TASK_PLANNED_DATE,
+        )
+
+        assertStatus(status, HTTP_201_CREATED)
+        assertTaskWithDict(task, **FIRST_TASK)
